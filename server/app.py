a0ccab269d8bd28132ee2aa5a41391a592ddf01a
Aditi Singh <211120224+aditiisingh0@users.noreply.github.com>
2026-04-01 22:37:40 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from env import CyberSecurityEnv
+import uvicorn
 
 app = FastAPI()
 env = CyberSecurityEnv()
@@ -24,3 +25,9 @@ def step(action: str):
 @app.get("/state")
 def state():
     return env.state().dict()
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
