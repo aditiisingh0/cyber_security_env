79224a4137451b39355241d641412a9a8bfc3d50
Aditi Singh <211120224+aditiisingh0@users.noreply.github.com>
2026-04-10 00:34:09 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,14 +1,17 @@
 tasks = [
     {
         "name": "easy",
-        "description": "Detect and remove malware"
+        "description": "Detect and remove malware",
+        "grader": "grade_easy"
     },
     {
         "name": "medium",
-        "description": "Stop DDoS attack"
+        "description": "Stop DDoS attack",
+        "grader": "grade_medium"
     },
     {
         "name": "hard",
-        "description": "Protect system for 50 steps"
+        "description": "Protect system for 50 steps",
+        "grader": "grade_hard"
     }
 ]
